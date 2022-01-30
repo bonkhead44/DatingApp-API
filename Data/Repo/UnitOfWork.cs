48a4ebe544ae15df5cfc3709c81c40c384517832
bonkhead44 <reazshovon44@gmail.com>
2022-01-30 22:22:30 +0600

    first commit

---
+++ Data/Repo/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using DatingApp_Api.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp_Api.Data.Repo
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDBContext _dBContext;
+        public UnitOfWork(ApplicationDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        public async Task<bool> SaveChanges()
+        {
+            return await _dBContext.SaveChangesAsync() > 0; 
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+using DatingApp_Api.Data;
+using DatingApp_Api.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddCors();
+            services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DbConnectionString")));
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
